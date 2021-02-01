e03efff95a69b4e56e1fc078de6bd48878725469
hgeng4 <hgeng4@illinois.edu>
2021-02-01 15:31:27 -0600

    debug server/client.py

---
+++ plot_scripts/compare_aur.py
@@ -0,0 +1,55 @@
+import glob
+import os
+
+import matplotlib.pyplot as plt
+import torch
+from sklearn import metrics
+
+L = 'L_1'
+phi = 'phi_0'
+dirs = {
+    'DFT': '/home/hgeng4/pmsp/results/Fmethod_fft/detection_ml/' + phi + '/N_16/' + L + '/inde_noise_level_0.5',
+    'DHT': '/home/hgeng4/pmsp/results/Fmethod_fht/detection_ml/' + phi + '/N_16/' + L + '/inde_noise_level_0.5',
+    'J-DHT': '/home/hgeng4/pmsp/results/Fmethod_fht_Jitter/detection_ml/' + phi + '/N_16/' + L + '/inde_noise_level_0.5',
+    'D-DHT': '/home/hgeng4/pmsp/results/Fmethod_fht_ditter/detection_ml/' + phi + '/N_16/' + L + '/inde_noise_level_0.5'
+}
+
+results_dict = {
+    k: [] for k in dirs.keys()
+}
+
+plot_dir = os.path.join('/home/hgeng4/pmsp/results/plots', 'aur')
+if not os.path.exists(plot_dir):
+    os.makedirs(plot_dir)
+
+plt.figure(figsize=(10, 5))
+for name in results_dict.keys():
+    results_paths = glob.glob(os.path.join(dirs[name], '*tar'))
+    results = []
+    for p in results_paths:
+        result = torch.load(p).get('result')
+        results.append(result)
+    print('collected {} results'.format(dirs[name]))
+    results_dict[name] = sorted(results, key=lambda x: x.usr_configs.signal.freqs[0])
+
+    areas = []
+    freqs = []
+    ks = []
+    for idx, result in enumerate(results):
+        area = metrics.auc(result.roc.fpr, result.roc.tpr)
+        areas.append(area)
+
+        f0 = result.usr_configs.signal.freqs[0]
+        k0 = result.usr_configs.signal.freqs[0] * result.usr_configs.signal.block_size / result.usr_configs.signal.fs
+
+        freqs.append(float(f0))
+        ks.append(k0)
+
+    plt.xlabel('$k_o$', fontsize=15)
+    plt.ylabel('AUR', fontsize=15)
+    plt.grid()
+    plt.legend(fontsize=15)
+    plt.tick_params('both', labelsize=15)
+    plt.savefig(os.path.join(plot_dir, 'aur.png'))
+    plt.clf()
+    plt.close()
