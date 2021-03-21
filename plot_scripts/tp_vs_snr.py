5fb23546f9367b9fbf9abf064af24d8d4357524b
Hanfei Geng <rexsasori@Hanfeis-MacBook-Pro.local>
2021-03-21 17:05:31 -0500

    plot

--- plot_scripts/tp_vs_snr.py
+++ plot_scripts/tp_vs_snr.py
@@ -15,12 +15,10 @@ def find_tp_rate(result, search_fpr=0.1):
     return roc_func(search_fpr)
 
 
-def tp_vs_snr(parent_dir, search_fpr=0.1):
+def tp_vs_snr(parent_dir, search_fpr=0.1, method='fft'):
     noise_level_dirs = glob.glob(os.path.join(parent_dir, '*'))
-
     noise_levels = []
     noise_level_tprs = {}
-
     tprs = []
 
     freqs = []
@@ -48,7 +46,8 @@ def tp_vs_snr(parent_dir, search_fpr=0.1):
                 fs = r.usr_configs.signal.fs
                 phase = r.usr_configs.signal.phases[0]
                 N = r.usr_configs.signal.block_size
-                power = utils.dft_output_signal_power(freq, phase, fs, N)
+                signal_power_kernel = utils.__SIGNAL_POWER__.get(method)
+                power = signal_power_kernel(freq, phase, fs, N)
                 freqs.append(freq)
                 compute_output_powers.append(power)
                 flag = 1
@@ -73,14 +72,14 @@ def plot_tpr_vs_noise_level(noise_levels, tprs, freqs, compute_output_power, how
     k0s = np.linspace(3, 4, num_freqs)
     for idx, f in enumerate(range(num_freqs)):
         if idx % how_often == 0:
-            plt.plot(10 * np.log10(compute_output_power[idx]) - 10 * np.log10(noise_levels / N/ L), tprs[:, idx],
+            plt.plot(10 * np.log10(compute_output_power[idx]) - 10 * np.log10(noise_levels / N / L), tprs[:, idx],
                      marker='o', markersize=5,
                      label='$k_o$ ' + str(k0s[idx]))
 
     plt.grid()
     plt.xlabel('Detector Input SNR(dB)', fontsize=15)
     plt.ylabel('$p_{tp}$', fontsize=15)
-    plt.title('fpr constraint: {}'.format(fpr_subj),fontsize=15)
+    plt.title('fpr constraint: {}'.format(fpr_subj), fontsize=15)
     plt.tick_params('both', labelsize=15)
     plt.legend(loc='lower right', fontsize=15)
     plt.ylim([-0.1, 1.1])
@@ -108,11 +107,69 @@ def plot_tpr_vs_noise_level(noise_levels, tprs, freqs, compute_output_power, how
     plt.close()
 
 
+def dht_vs_dft_different_snr(dft_data, dht_data):
+    num_freqs = dft_plot_data.tprs.shape[1]
+    print(num_freqs)
+    plt.figure(figsize=(10, 5))
+    k0s = np.linspace(3, 4, num_freqs)
+
+    for idx, f in enumerate(range(num_freqs)):
+        if idx % dft_data.how_often == 0:
+            plt.plot(-10 * np.log10(dft_data.noise_levels / N / L), dft_data.tprs[:, idx],
+                     marker='o', markersize=5,
+                     label='$k_o$ ' + str(k0s[idx]) + ' method: ' + dft_data.method)
+
+    for idx, f in enumerate(range(num_freqs)):
+        if idx % dht_data.how_often == 0:
+            plt.plot(-10 * np.log10(dht_data.noise_levels / N / L), dht_data.tprs[:, idx],
+                     marker='o', markersize=5,
+                     label='$k_o$ ' + str(k0s[idx]) + ' method: ' + dht_data.method)
+
+    plt.grid()
+    plt.xlabel('Input SNR(dB)', fontsize=15)
+    plt.ylabel('$p_{tp}$', fontsize=15)
+
+    plt.tick_params('both', labelsize=15)
+    plt.legend(loc='lower right', fontsize=15,ncol=2)
+    plt.savefig('/home/hgeng4/pmsp/plots/tpr_snr.png')
+    plt.clf()
+    plt.close()
+
+
+class PlotData:
+    def __init__(self, noise_levels, tprs, freqs,
+                 compute_output_power, how_often=25,
+                 fpr_subj=0.05, N=16, fs=2000, L=1, method='DFT'):
+        self.noise_levels = noise_levels
+        self.tprs = tprs
+        self.freqs = freqs
+        self.compute_out_power = compute_output_power
+        self.how_often = how_often
+        self.fpr_subj = fpr_subj
+        self.N = N
+        self.fs = fs
+        self.L = L
+        self.method = method
+
+
 if __name__ == '__main__':
     fpr_subj = 0.05
     fs = 2000
     N = 16
-    L = 10
-    dirname = '/home/hgeng4/pmsp/results/Fmethod_fft/detection_ml/phi_0/N_16/L_'+str(L)
-    noise_levels, tprs, freqs, out_power = tp_vs_snr(dirname, search_fpr=fpr_subj)
-    plot_tpr_vs_noise_level(noise_levels, tprs, freqs, out_power, fpr_subj=fpr_subj, N=N, fs=fs, L=L)
+    L = 1
+    fft_dirname = '/home/hgeng4/pmsp/results/Fmethod_fft/detection_ml/phi_0.7853981633974483/N_16/L_' + str(L)
+    fht_dirname = '/home/hgeng4/pmsp/results/Fmethod_fht/detection_ml/phi_0.7853981633974483/N_16/L_' + str(L)
+    noise_levels_fft, tprs_fft, freqs_fft, out_power_fft = tp_vs_snr(fft_dirname, fpr_subj, 'fft')
+    noise_levels_fht, tprs_fht, freqs_fht, out_power_fht = tp_vs_snr(fht_dirname, fpr_subj, 'fht')
+
+    dft_plot_data = PlotData(
+        noise_levels=noise_levels_fft, tprs=tprs_fft, freqs=freqs_fft,
+        compute_output_power=out_power_fft, how_often=25,
+        fpr_subj=0.05, N=16, fs=2000, L=1, method='DFT'
+    )
+
+    dht_plot_data = PlotData(
+        noise_levels=noise_levels_fht, tprs=tprs_fht, freqs=freqs_fht,
+        compute_output_power=out_power_fht, how_often=25,
+        fpr_subj=0.05, N=16, fs=2000, L=1, method='DHT'
+    )
