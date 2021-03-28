f5686ff13de8d1a6bc15ec4c27f50200bb0ff61f
Hanfei Geng <rexsasori@Hanfeis-MacBook-Pro.local>
2021-03-28 14:10:32 -0500

    ji

--- plot_scripts/tp_vs_snr.py
+++ plot_scripts/tp_vs_snr.py
@@ -187,7 +187,7 @@ if __name__ == '__main__':
     fs = 2000
     N = 16
     L = [1, 2, 5, 10]
-    compare_k0 = 3.5
+    compare_k0 = 3.75
 
     fft_dirnames = ['/home/hgeng4/THESIS/results/Fmethod_fft/detection_ml/phi_0.7853981633974483/N_16/L_' + str(l) for l
                     in L]
