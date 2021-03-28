4defb45b39c3fc48dd1ad2f803ac00de6a520007
Hanfei Geng <rexsasori@Hanfeis-MacBook-Pro.local>
2021-03-28 03:15:01 -0500

    each bin 1/sigma^2

--- core/signal_generator.py
+++ core/signal_generator.py
@@ -69,7 +69,7 @@ class InputSignalGenerator:
             ret += amp * np.cos(2 * np.pi * freq * self.t + phase)
 
         # normalize the signal such that each block has power of 1
-        ret = self.N * self.L * ret / (ret**2).sum()
+        ret = self.N * self.N * self.L * ret / (ret**2).sum()
         ret = dsputils.reformat(ret, self.observation_block_size, self.hop_size, self.num_pos_sample)
         return ret
 
